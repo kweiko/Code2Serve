9aec14fd388cb2895611b8cc8eec6c53950614a7
kweiko <kweiko@hotmail.com>
2020-04-15 00:01:34 +0100

    Fixed navbar & vid section. Added white logo.

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -15,10 +15,10 @@ $(function () {
         var scroll = $(window).scrollTop();
         if (scroll < 20) {
             $(".navbar-area").removeClass("sticky");
-            $(".navbar .navbar-brand img").attr("src", "assets/images/logo.svg");
+            $(".navbar .navbar-brand img").attr("src", "assets/images/GYDE_white_200-56.png");
         } else {
             $(".navbar-area").addClass("sticky");
-            $(".navbar .navbar-brand img").attr("src", "assets/images/logo-2.svg");
+            $(".navbar .navbar-brand img").attr("src", "assets/images/GYDE_200-56.png");
         }
     });
 
